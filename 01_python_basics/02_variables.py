3249b63cfbbd2f0114423168c123fbebfd6847a3
Vaidik118 <vaidikvala10@gmail.com>
2026-09-25 09:19:47 +0530

    add variables practice

---
+++ 01_python_basics/02_variables.py
@@ -0,0 +1,18 @@
+name = "Vaidik Vala"
+
+age = 23
+
+is_student = True
+
+height = 6.0
+
+print(name)
+print(age)
+print(is_student)
+print(height)
+
+
+print("My name is",name)
+print("My age is",age)
+print("My height is",height)
+print("Am i Student?",is_student)
