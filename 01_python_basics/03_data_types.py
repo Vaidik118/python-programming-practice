b10ef6c32ebb049b4bc901c75ffdd5facf94f59e
Vaidik118 <vaidikvala10@gmail.com>
2026-09-25 09:37:43 +0530

    add data type practice

---
+++ 01_python_basics/03_data_types.py
@@ -0,0 +1,12 @@
+number = 10
+
+square = number ** 2
+
+cube = number ** 3
+
+print("square:",square)
+print ("cube:",cube)
+
+print("data type of number:", type(number))
+print("data type square:", type(square))
+print("type of cube:", type(cube))
