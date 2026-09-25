a21043f4a89c7e21a206310dd6ee975a10253b4c
Vaidik118 <vaidikvala10@gmail.com>
2026-09-25 09:47:08 +0530

    add arithmetic operators practice

---
+++ 01_python_basics/04_arithmetic_operators.py
@@ -0,0 +1,15 @@
+num1 = 10
+num2 = 4
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Number1:",num1)
+print("Number2:",num2)
+
+print("Addition:",addition)
+print("Subtraction:",subtraction)
+print("Multiplication:",multiplication)
+print("Division:",division)
