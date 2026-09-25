39c4a9cd4ee4a79f8f209f749faa072299671098
Vaidik118 <vaidikvala10@gmail.com>
2026-09-25 10:04:08 +0530

    add averahe calculation practice

---
+++ 01_python_basics/05_average_calculation.py
@@ -0,0 +1,13 @@
+day1 = 10
+day2 = 14
+day3 = 32
+day4 = 7
+day5 = 8
+day6 = 10
+
+total = day1 + day2 + day3 + day4 + day5 + day6
+
+average = total/3
+
+print("Total:",total)
+print("Average:",average)
