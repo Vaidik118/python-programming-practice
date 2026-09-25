236d672a8a1a3b4628ae0a8d0256ed0f1c5a9bd2
Vaidik118 <vaidikvala10@gmail.com>
2026-09-25 10:19:42 +0530

    Add type casting practice

---
+++ 01_python_basics/07_type_casting.py
@@ -0,0 +1,11 @@
+pi = float(input("Please enter your pi: "))
+radius = int(input("Please enter your radius: "))
+
+area = pi * radius * radius
+
+print("Radius:", radius)
+print("Area:", area)
+
+print("type of pi:", type(pi))
+print("type of radius:", type(radius))
+print("type of area:", type(area))
