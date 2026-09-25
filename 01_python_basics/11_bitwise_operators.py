60025edcd9ca97d9974c75e527722aa4c300e865
Vaidik118 <vaidikvala10@gmail.com>
2026-09-25 11:25:23 +0530

    Add bitwise operators practice

---
+++ 01_python_basics/11_bitwise_operators.py
@@ -0,0 +1,10 @@
+#binary representation of integers.
+a = 10
+b = 6
+
+print("a & b =", a & b)
+print("a | b =", a | b)
+print("a ^ b =", a ^ b)
+print("~a =", ~a)
+print("a << 1 =", a << 1)
+print("a >> 1 =", a >> 1)
